7f159513cced61eddaf94ff4f5afafb79a99fcd8
azarrias <adolfo.zarrias@gmail.com>
2022-01-16 23:21:36 +0100

    Adjust canvas scaler and set text anchors to adapt to other aspect ratios

---
+++ glitch-garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private int currentSceneIndex;
+    private const int SECONDS_TO_WAIT = 4; 
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForSeconds());
+        }
+    }
+
+    private IEnumerator WaitForSeconds()
+    {
+        yield return new WaitForSeconds(SECONDS_TO_WAIT);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
