a155e064f8baa2d960d8d4571bf78b3eb0298910
azarrias <adolfo.zarrias@gmail.com>
2022-01-20 18:51:01 +0100

    Make a trophy bounce key frame animation and make attackers move

---
+++ glitch-garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 5f)]
+    [SerializeField] private float walkSpeed = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime);
+    }
+}
