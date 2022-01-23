2827d21ac2f0efc07de4ad997dd016057f233141
azarrias <adolfo.zarrias@gmail.com>
2022-01-23 11:49:58 +0100

    Create prefabs and add attacker spawners to level

---
+++ glitch-garden/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] private float minSpawnDelay = 1f;
+    [SerializeField] private float maxSpawnDelay = 5f;
+    [SerializeField] private Attacker attackerPrefab;
+    private bool spawning = true;
+
+    IEnumerator Start()
+    {
+        while (spawning)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    void Update()
+    {
+        
+    }
+}
